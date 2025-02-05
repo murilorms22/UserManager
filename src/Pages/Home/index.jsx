cf9b0d152a608d60b858235bc5880fe3d4b82c27
Murilo <murilorms22@gmail.com>
2025-02-05 11:56:44 -0300

    segundo dia de projeto

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,28 +1,48 @@
+import { useEffect, useState } from 'react'
 import './styles.css'
 import Lixo from '../../assets/trash.svg'
+import api from '../../services/api'
 
 function Home() {
+  const [usuarios, setUsuarios] = useState([])
+  
+
+  async function getUsuarios(){
+    const usuariosApi = await api.get('/usuarios')
+  
+    setUsuarios(usuariosApi.data)
+  }
+
+  useEffect(() => {
+    getUsuarios()
+  }, [])
 
   return (
     <div className='Home'>
-    <div className='container'>
-      <h1>Cadastro de usuário</h1>
-      <form>
-        <input type="text" placeholder='Nome' />
-        <input type="number" placeholder='Idade' />
-        <input type="email" placeholder='E-mail' />
-        <input type="submit" />
-      </form>
-    </div>
-
-    <div className='usuarios-cadastrados'>
-      <div className='infos'>
-      <p>Nome: </p>
-      <p>Idade: </p>
-      <p>E-mail: </p>
+      <div className='container'>
+        <h1>Cadastro de usuário</h1>
+        <form>
+          <input type="text" placeholder='Nome' />
+          <input type="number" placeholder='Idade' />
+          <input type="email" placeholder='E-mail' />
+          <input type="submit" />
+        </form>
       </div>
-      <img src={Lixo} />
-    </div>
+
+      {usuarios.map((usuario) => (
+
+        <div className='usuarios-cadastrados' key={usuario.id}>
+          <div className='infos'>
+            <p>Nome: <span>{usuario.name}</span></p>
+            <p>Idade: <span>{usuario.age}</span></p>
+            <p>E-mail: <span>{usuario.email}</span></p>
+          </div>
+          <img src={Lixo} />
+        </div>
+
+      ))}
+
+
     </div>
   )
 }
